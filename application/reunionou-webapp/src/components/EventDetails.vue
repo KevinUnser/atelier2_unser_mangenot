5bbe76b044e06b5b7cd082f5204e4aca00dc8a3d
Mangenot-alex <alex.mangenot6@etu.univ-lorraine.fr>
2022-03-30 11:10:31 +0200

    front "basique" fini
    front "basique" fini, tous les éléments souhaiter sont ajouter, avec un style.
    
    à venir : adapter l'affichage en fonction des données.

--- application/reunionou-webapp/src/components/EventDetails.vue
+++ application/reunionou-webapp/src/components/EventDetails.vue
@@ -35,12 +35,48 @@ export default {
     </div>
 
     <div id="div_participants">
-      <ul>
-        <li>Roger</li>
-        <li>Béa</li>
-        <li>Anes</li>
-        <li>Jule</li>
-      </ul>
+      <h3 id="h3_pre">Participant</h3>
+
+      <div id="div_pre">
+        <li><strong>Roger</strong> - je serais là à partir de 14h30</li>
+        <li><strong>Béa</strong></li>
+      </div>
+
+      <h3 id="h3_int">intéressé</h3>
+
+      <div id="div_int">
+        <li><strong>Anes</strong> - je sais pas encore quand je pars en vacances</li>
+        <li><strong>Jules</strong> - je ne saurais qu'au dernier moment si je suis dispo</li>
+      </div>
+
+      <h3 id="h3_abs">Absent</h3>
+
+      <div id="div_abs">
+        <li><strong>Mathilde</strong> - j'ai le covid</li>
+      </div>
+
+      <h3 id="h3_att">En attentes</h3>
+
+      <div id="div_att">
+        <li><strong>Xavier</strong></li>
+        <li><strong>Daniel</strong></li>
+      </div>
+    </div>
+
+    <div id="div_comment">
+      <h2>Commentaires</h2>
+      <div class="div_one_com">
+        <p class="p_nom">Roger</p>
+        <p class="p_com">super soiré, on remet ça quand vous voulez</p>
+      </div>
+      <div class="div_one_com">
+        <p class="p_nom">Xavier</p>
+        <p class="p_com">On prendra moins d'alcool la prochaine fois</p>
+      </div>
+      <div class="div_one_com">
+        <p class="p_nom">Daniel</p>
+        <p class="p_com">Désolé au final j'ai eu un imprévue.</p>
+      </div>
     </div>
   </div>
 </template>
@@ -83,14 +119,63 @@ export default {
   margin-top: 10%;
 }
 
-#div_reponse{
-    width: 100%;
-    margin-top: 30%;
+#div_reponse {
+  width: 100%;
+  margin-top: 30%;
 }
 
-.btn{
-    width: 60%;
-    margin: 0.2em;
+.btn {
+  width: 60%;
+  margin: 0.2em;
+}
+
+#div_participants {
+  margin: 5em 5% 5em 5%;
+  text-align: center;
+}
+
+#div_pre,
+#h3_pre {
+  color: darkgreen;
+}
+
+#div_int,
+#h3_int {
+  color: darkblue;
+  margin-top: 0.5em;
+}
+
+#div_abs,
+#h3_abs {
+  color: darkred;
+  margin-top: 0.5em;
+}
+
+#div_att,
+#h3_att {
+  color: gray;
+  margin-top: 0.5em;
+}
+
+li {
+  list-style: none;
+}
+
+#div_comment{
+  margin: 0% 5% 3em 5%;
+}
+
+.div_one_com{
+  display: flex;
+}
+
+.p_nom{
+  width: 20%;
+  margin-right: 0.5em;
+}
+
+.p_com{
+  width: 80%;
 }
 </style>
 
--- application/reunionou-webapp/src/views/EventsView.vue
+++ application/reunionou-webapp/src/views/EventsView.vue
@@ -25,20 +25,20 @@ export default {
   <div class="mt-3">
     <div id="div_myEvents">
       <h3>Mes évenements</h3>
-      <RouterLink to="/event"><EventR @click="getId(1)"/></RouterLink>
-      <RouterLink to="/event"><EventR @click="getId(2)"/></RouterLink>
+      <RouterLink class="rtlk_eventR" to="/event"><EventR @click="getId(1)"/></RouterLink>
+      <RouterLink class="rtlk_eventR" to="/event"><EventR @click="getId(2)"/></RouterLink>
     </div>
 
     <div id="div_shareEvents">
       <h3>Evenements partager avec moi</h3>
-      <RouterLink to="/event"><EventR @click="getId(3)"/></RouterLink>
-      <RouterLink to="/event"><EventR @click="getId(4)"/></RouterLink>
-      <RouterLink to="/event"><EventR @click="getId(5)"/></RouterLink>
+      <RouterLink class="rtlk_eventR" to="/event"><EventR @click="getId(3)"/></RouterLink>
+      <RouterLink class="rtlk_eventR" to="/event"><EventR @click="getId(4)"/></RouterLink>
+      <RouterLink class="rtlk_eventR" to="/event"><EventR @click="getId(5)"/></RouterLink>
     </div>
     <div id="div_pubEvents">
       <h3>Evenements public</h3>
-      <RouterLink to="/event"><EventR @click="getId(6)"/></RouterLink>
-      <RouterLink to="/event"><EventR @click="getId(7)"/></RouterLink>
+      <RouterLink class="rtlk_eventR" to="/event"><EventR @click="getId(6)"/></RouterLink>
+      <RouterLink class="rtlk_eventR" to="/event"><EventR @click="getId(7)"/></RouterLink>
     </div>
   </div>
 </template>
@@ -47,5 +47,10 @@ export default {
 
 #div_myEvents, #div_shareEvents, #div_pubEvents{
     margin-inline: 10%;
+}
+
+.rtlk_eventR{
+  text-decoration: none;
+  color: black;
 }
 </style>
